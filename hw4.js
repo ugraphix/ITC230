085e6696f852f234ecf1207bc9293071038873a6
Gracie <grace@ugraphix.com>
2017-05-14 16:25:32 -0700

    Add files via upload

---
+++ hw4.js
@@ -0,0 +1,12 @@
+//eslint -c .eslintsrc.js hw4.js
+
+
+// eslint practice file for ITC 230
+const names = ['sara', 'joe', 'dave', 'ann'];
+names.map((item) =>{
+ return item.toUpperCase();
+ 
+});
+//console.log(names);
+
+
