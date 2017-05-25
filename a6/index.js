2f7a5a5c52190fefac5c6bc4746122a6c71d5e9e
Gracie <grace@ugraphix.com>
2017-05-24 19:03:39 -0700

    Add files via upload
    fix DB

--- a6/index.js
+++ a6/index.js
@@ -3,14 +3,16 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
-var school = require('./lib/school.js');
 var Course = require("./models/course.js")
 
+//var routes = require('./routes.js')(app); // pass ‘app’ instance to the routes module
 
 //link body parser for form handling
 app.use(require("body-parser").urlencoded({extended: true}));
 
 app.use(express.static('public'));
+
+app.use('/api', require('cors')()); // set Access-Control-Allow-Origin header for api route
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
@@ -55,20 +57,69 @@ app.get('/about', function(req, res) {
 app.get('/get', function(req, res) {
     Course.findOne({course: req.query.course}, (err, result) => {
        res.render('details', {course: req.query.course, item: result });
-    });          
+    });  
+          
 });
 
 app.post('/get', function(req, res) {
     Course.findOne({course: req.body.course}, (err, result) => {
        res.render('details', {course: req.body.course, item: result });
-    });          
+    });  
 });
 
 //delete object
 app.get('/delete', function(req, res) {
-    var result = school.delete(req.query.course)
-    res.send("Deleted course " + req.query.course + "\n" + JSON.stringify(result));
+    Course.remove({title:req.query.course}, (err,result) => {
+    res.send("Deleted course " + req.query.course + "\n" + JSON.stringify(result));                  
+    })
+
 });
+
+
+
+//api's
+app.get('/api/v1/course/:course', (req, res, next) => {
+    let course = req.params.course;
+    Course.findOne({course: course}, (err, result) => {
+        if (err || !result) return next(err);
+        res.json( result );    
+    });
+});
+
+app.get('/api/v1/courses', (req,res, next) => {
+    Course.find((err,results) => {
+        if (err || !results) return next(err);
+        res.json(results);
+    });
+});
+
+
+app.get('/api/v1/delete/:course', (req,res, next) => {
+    Course.remove({"course":req.params.course }, (err, result) => {
+        if (err) return next(err);
+        // return # of items deleted
+        res.json({"deleted": result.result.n});
+    });
+});
+
+
+app.get('/api/v1/add/:course/:teacher/:code', (req,res, next) => {
+    // find & update existing item, or add new 
+    let course = req.params.course;
+    Course.update({ course: course}, {course:course, teacher: req.params.teacher, code: req.params.code }, {upsert: true }, (err, result) => {
+        if (err) return next(err);
+        // nModified = 0 for new item, = 1+ for updated item 
+        res.json({updated: result.nModified});
+    });
+});
+
+
+
+
+
+
+
+
 
 
 
